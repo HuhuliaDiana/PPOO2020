904160b51c56721c20f404ee1d69b9e50237d562
Cristian Ciurea <cristianciurea@gmail.com>
2020-10-28 16:51:47 +0200

    update

---
+++ course10/javaFX/JavaFXHelloWorld.java
@@ -0,0 +1,17 @@
+package examples.course10.javaFX;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+ 
+public class JavaFXHelloWorld extends Application {
+ 
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
