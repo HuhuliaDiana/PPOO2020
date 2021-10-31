e84afff6d0e2318c9beeb81ba5d90b63d29f34e4
CRISTIAN CIUREA <cristianciurea@gmail.com>
2021-10-31 21:39:47 +0200

    update

---
+++ PPOO2021/JavaFXApp1/src/Login.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login {
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton autentificareButton;
+
+    public Login() {
+        autentificareButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
---
+++ PPOO2021/JavaFXcurs11/src/FxFXMLExample2.java
@@ -0,0 +1,39 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class FxFXMLExample2 extends Application
+{
+	public static void main(String[] args)
+	{
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws IOException
+	{
+		// Create the FXMLLoader
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("FxFXMLExample3.fxml"));
+		// Path to the FXML File
+		String fxmlDocPath = "C:\\Users\\Administrator\\workspace\\JavaFXcurs11\\src/FxFXMLExample2.fxml";
+		FileInputStream fxmlStream = new FileInputStream(fxmlDocPath);
+
+		// Create the Pane and all Details
+		VBox root = (VBox) loader.load(fxmlStream);
+
+		// Create the Scene
+		Scene scene = new Scene(root);
+		// Set the Scene to the Stage
+		stage.setScene(scene);
+		// Set the Title to the Stage
+		stage.setTitle("A FXML Example with a Script Event Handler");
+		// Display the Stage
+		stage.show();
+
+	}
+}
